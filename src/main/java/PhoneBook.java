aeee93ff224a703fb2f186bee2e92269aa8d090f
Dmitry Remizov <rdk.7777@yahoo.com>
2023-07-13 11:55:58 +0300

    Stub and tests for add

--- src/main/java/PhoneBook.java
+++ src/main/java/PhoneBook.java
@@ -1,2 +1,15 @@
+import java.util.Collection;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
+
 public class PhoneBook {
-}
+    static Map<String, Integer> phoneBook = new TreeMap<>();
+    public static Map<String, Integer> getPhoneBook() {
+        return phoneBook;
+    }
+    public static int add (String name, int phoneNumber) {
+        return 0;
+        }
+    }
+
--- src/test/java/PhoneBookTest.java
+++ src/test/java/PhoneBookTest.java
@@ -1,2 +1,16 @@
+import java.util.Map;
+import java.util.TreeMap;
+
 public class PhoneBookTest {
+
+    static Map<String, Integer> phoneBook = new TreeMap<>();
+    public static Map<String, Integer> getPhoneBook() {
+        return phoneBook;
+    }
+    public static int add(String name, int phoneNumber) {
+        if (!phoneBook.containsKey(name)) {
+            phoneBook.putIfAbsent(name, phoneNumber);
+        }
+        return phoneBook.size();
+    }
 }
